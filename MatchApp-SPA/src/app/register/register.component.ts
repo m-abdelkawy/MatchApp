7b21e4a15038acf6eb2f55887e76b354986c38fa
Mohammed.abdelkawy@diroots.com <mohammed.abdelkawy@diroots.com>
2020-06-09 02:54:09 +0200

    End of Section 6 - adding 3rd party components

--- MatchApp-SPA/src/app/register/register.component.ts
+++ MatchApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -19,14 +20,14 @@ export class RegisterComponent implements OnInit {
   register(){
     //debugger;
     this.authService.register(this.model).subscribe(()=>{
-      console.log("Registered Successfully!");
+      this.alertify.success("Registered Successfully!");
     }, error=>{
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel(){
     this.cancelRegister.emit(false);
-    console.log('cancelled! - register component');
+    this.alertify.message('cancelled! - register component');
   }
 }
